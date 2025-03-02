c1573d69e3e1e1fb1cb3292ced6f1a61fc4d04a7
Rod MP <ramp.bng@gmail.com>
2025-03-01 23:08:13 -0600

    fix __dir

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,17 +1,9 @@
 import mongoose from 'mongoose';
-import { DataSource } from 'typeorm';
-
-const AppDataSource = new DataSource({
-  type: 'mongodb',
-  url: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks',
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 
 const initializeDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
-    await AppDataSource.initialize();
+    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+    });
     console.log('Connected to the database');
   } catch (error) {
     console.log('Database connection error:', error);
